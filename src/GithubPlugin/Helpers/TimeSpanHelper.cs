26bc4345f6005f3c4b112a9bcfd4145ad05e0364
Laszlo Nemeth <57342539+donlaci@users.noreply.github.com>
2023-05-10 16:41:58 -0700

    Nl candidate2 additional GitHub widgets (#43)
    Co-authored-by: Laszlo Nemeth <v-lanemeth@microsoft.com>

---
+++ src/GithubPlugin/Helpers/TimeSpanHelper.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+using DevHome.Logging;
+
+namespace GitHubPlugin.Helpers;
+internal class TimeSpanHelper
+{
+    public static string TimeSpanToDisplayString(TimeSpan timeSpan, Logger? log = null)
+    {
+        if (timeSpan.TotalSeconds < 1)
+        {
+            return Resources.GetResource("WidgetTemplate_Now", log);
+        }
+
+        if (timeSpan.TotalSeconds < 2)
+        {
+            return $"1 {Resources.GetResource("WidgetTemplate_SecondAgo", log)}";
+        }
+
+        if (timeSpan.TotalMinutes < 1)
+        {
+            return $"{timeSpan.Seconds} {Resources.GetResource("WidgetTemplate_SecondsAgo", log)}";
+        }
+
+        if (timeSpan.TotalMinutes < 2)
+        {
+            return $"1 {Resources.GetResource("WidgetTemplate_MinuteAgo", log)}";
+        }
+
+        if (timeSpan.TotalHours < 1)
+        {
+            return $"{timeSpan.Minutes} {Resources.GetResource("WidgetTemplate_MinutesAgo", log)}";
+        }
+
+        if (timeSpan.TotalHours < 2)
+        {
+            return $"1 {Resources.GetResource("WidgetTemplate_HourAgo", log)}";
+        }
+
+        if (timeSpan.TotalDays < 1)
+        {
+            return $"{timeSpan.Hours} {Resources.GetResource("WidgetTemplate_HourssAgo", log)}";
+        }
+
+        if (timeSpan.TotalDays < 2)
+        {
+            return $"1 {Resources.GetResource("WidgetTemplate_DayAgo", log)}";
+        }
+
+        return $"{timeSpan.Days} {Resources.GetResource("WidgetTemplate_DaysAgo", log)}";
+    }
+}
