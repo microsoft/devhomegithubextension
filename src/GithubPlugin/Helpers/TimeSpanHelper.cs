3ec5ac1f8f4313cad878b1490f074d4ce160d739
Eric Johnson <ericjohnson327@gmail.com>
2023-06-15 15:23:38 -0700

    Undoing fixes from different PR

--- src/GithubPlugin/Helpers/TimeSpanHelper.cs
+++ src/GithubPlugin/Helpers/TimeSpanHelper.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 using System.Text.Json.Nodes;
 using DevHome.Logging;
-using Jeffijoe.MessageFormat;
 
 namespace GitHubPlugin.Helpers;
 internal class TimeSpanHelper
