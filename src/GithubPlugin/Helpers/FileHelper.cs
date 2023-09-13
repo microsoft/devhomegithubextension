687187a47e46b5383174f0b076b57299a1e3b644
Eric Johnson <ericjohnson327@gmail.com>
2023-09-13 10:54:46 -0700

    SettingsProvider (#220)
    * Everything builds.
    
    * ALmost activating a responce object
    
    * Refrencing correct dll
    
    * SDK Changes (#217)
    
    * SDK Changes
    
    * Oops
    
    * More changes
    
    * Forgot a csproj
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    
    * Add settings provider
    
    * Update settings provider for new SDK
    
    * Fix merge conflicts
    
    * Readd missing dependencies
    
    * CR feedback
    
    * CR feedback
    
    ---------
    
    Co-authored-by: Darren Hoehna <dahoehna@microsoft.com>
    Co-authored-by: Darren Hoehna <dhoehna@yahoo.com>

---
+++ src/GithubPlugin/Helpers/FileHelper.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DevHome.Common.Services;
+
+public static class FileHelper
+{
+#pragma warning disable CS8603 // Possible null reference return.
+    public static T Read<T>(string folderPath, string fileName)
+    {
+        var path = Path.Combine(folderPath, fileName);
+        if (File.Exists(path))
+        {
+            var json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        return default;
+    }
+#pragma warning restore CS8603 // Possible null reference return.
+
+    public static void Save<T>(string folderPath, string fileName, T content)
+    {
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        var fileContent = JsonConvert.SerializeObject(content);
+        File.WriteAllText(Path.Combine(folderPath, fileName), fileContent, Encoding.UTF8);
+    }
+
+    public static void Delete(string folderPath, string fileName)
+    {
+        if (fileName != null && File.Exists(Path.Combine(folderPath, fileName)))
+        {
+            File.Delete(Path.Combine(folderPath, fileName));
+        }
+    }
+}
