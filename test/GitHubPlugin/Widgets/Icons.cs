12f9c9b82272c2d3202fa9ffd65de924c035b6a8
Darren Hoehna <dahoehna@microsoft.com>
2023-09-13 12:48:48 -0700

    Maybe this'll work.

--- test/GitHubPlugin/Widgets/Icons.cs
+++ test/GitHubPlugin/Widgets/Icons.cs
@@ -9,10 +9,12 @@ public partial class WidgetTests
     [TestCategory("Unit")]
     public void IconsTest()
     {
+        /*
         using var log = new Logger("TestStore", TestOptions.LogOptions);
         var testListener = new TestListener("TestListener", TestContext!);
         log.AddListener(testListener);
         DataModel.Log.Attach(log);
+        */
 
         var icon = Helpers.IconLoader.GetIconAsBase64("arrow.png");
 
