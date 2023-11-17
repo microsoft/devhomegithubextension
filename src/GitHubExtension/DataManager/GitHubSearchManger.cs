84ca2ffb8a345e16540d756efc3118d695568bb8
Vineeth Thomas Alex <vineeththomasalex@gmail.com>
2023-11-17 10:31:49 -0800

    SearchManager minor update

--- src/GitHubExtension/DataManager/GitHubSearchManger.cs
+++ src/GitHubExtension/DataManager/GitHubSearchManger.cs
@@ -4,6 +4,7 @@
 using GitHubExtension.Client;
 using GitHubExtension.DataManager;
 using GitHubExtension.DataModel;
+using Microsoft.Windows.DevHome.SDK;
 
 namespace GitHubExtension;
 
@@ -30,6 +31,41 @@ public partial class GitHubSearchManager : IGitHubSearchManager, IDisposable
             Log.Logger()?.ReportError(Name, "Failed creating GitHubSearchManager", e);
             Environment.FailFast(e.Message, e);
             return null;
+        }
+    }
+
+    public async Task SearchForGitHubIssuesOrPRs(Octokit.SearchIssuesRequest request, string initiator, SearchCategory category, IDeveloperId developerId, RequestOptions? options = null)
+    {
+        Log.Logger()?.ReportInfo(Name, $"Searching for issues or pull requests for widget {initiator}");
+        request.State = Octokit.ItemState.Open;
+        request.Archived = false;
+        request.PerPage = 10;
+        request.SortField = Octokit.IssueSearchSort.Updated;
+        request.Order = Octokit.SortDirection.Descending;
+
+        var client = GitHubClientProvider.Instance.GetClient(developerId.Url) ?? throw new InvalidOperationException($"Client does not exist for {developerId.Url}");
+
+        // Set is: parameter according to the search category.
+        // For the case we are searching for both we don't have to set the parameter
+        if (category.Equals(SearchCategory.Issues))
+        {
+            request.Is = new List<Octokit.IssueIsQualifier>() { Octokit.IssueIsQualifier.Issue };
+        }
+        else if (category.Equals(SearchCategory.PullRequests))
+        {
+            request.Is = new List<Octokit.IssueIsQualifier>() { Octokit.IssueIsQualifier.PullRequest };
+        }
+
+        var octokitResult = await client.Search.SearchIssues(request);
+        if (octokitResult == null)
+        {
+            Log.Logger()?.ReportDebug($"No issues or PRs found.");
+            SendResultsAvailable(new List<Octokit.Issue>(), initiator);
+        }
+        else
+        {
+            Log.Logger()?.ReportDebug(Name, $"Results contain {octokitResult.Items.Count} items.");
+            SendResultsAvailable(octokitResult.Items, initiator);
         }
     }
 
