6e92d76d2991b9acaa332256182a52167a8773cc
David Bennett <dbennett-msft@outlook.com>
2023-12-12 10:47:16 -0800

    Force a build break

--- src/GitHubExtension/DataModel/DataStore.cs
+++ src/GitHubExtension/DataModel/DataStore.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.Reflection;
 using System.Text;
 using GitHubExtension.Helpers;
-using Microsoft.Data.Sqlite;
+using Microsoft.Data.Sqlite
 
 namespace GitHubExtension.DataModel;
 
