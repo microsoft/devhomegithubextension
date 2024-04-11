cfb143d74e5864fa50e8cc3100d07fcb21a3d3c0
Eric Johnson <ericjohnson327@gmail.com>
2024-04-11 09:31:51 -0700

    staching webview2 code

--- src/GitHubExtension/GitHubExtension.cs
+++ src/GitHubExtension/GitHubExtension.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Net;
 using System.Runtime.InteropServices;
 using GitHubExtension.DeveloperId;
 using GitHubExtension.Providers;
@@ -11,13 +12,24 @@ namespace GitHubExtension;
 [ComVisible(true)]
 [Guid("6B5F1179-B2AE-4D5E-94FC-E5E119D1B8F0")]
 [ComDefaultInterface(typeof(IExtension))]
-public sealed class GitHubExtension : IExtension
+public sealed class GitHubExtension : IExtension, IDisposable
 {
     private readonly ManualResetEvent _extensionDisposedEvent;
+
+    private readonly WebServer.WebServer _webServer;
 
     public GitHubExtension(ManualResetEvent extensionDisposedEvent)
     {
         _extensionDisposedEvent = extensionDisposedEvent;
+
+        var webcontentPath = Path.Combine(AppContext.BaseDirectory, "WebContent");
+        _webServer = new WebServer.WebServer(webcontentPath);
+
+        _webServer.RegisterRouteHandler("/api/test", HandleRequest);
+
+        Console.WriteLine($"GitHubExtension is running on port {_webServer.Port}");
+        var url = $"http://localhost:{_webServer.Port}/HelloWorld.html";
+        Console.WriteLine($"Navigate to: {url}");
     }
 
     public object? GetProvider(ProviderType providerType)
@@ -41,5 +53,12 @@ public sealed class GitHubExtension : IExtension
     public void Dispose()
     {
         _extensionDisposedEvent.Set();
+        _webServer.Dispose();
+    }
+
+    public bool HandleRequest(HttpListenerRequest request, HttpListenerResponse response)
+    {
+        Console.WriteLine("Received request for /api/test");
+        return true;
     }
 }
---
+++ src/GitHubExtension/WebContent/HelloWorld.js
@@ -0,0 +1,4 @@
+function LoadPage()
+{
+    document.getElementById("contentWrapper").innerHTML = "Hello World!  This is a test.";
+}
