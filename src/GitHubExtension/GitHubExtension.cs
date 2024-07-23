276e9c41d5f4512729aef126faae7bb704ae4a67
Eric Johnson <ericjohnson327@gmail.com>
2024-07-23 10:40:00 -0700

    Fix duplicate classIDs for different builds (#418)
    * Fix duplicate classIDs for different builds
    
    * Fix duplicate classIDs for different builds

--- src/GitHubExtension/GitHubExtension.cs
+++ src/GitHubExtension/GitHubExtension.cs
@@ -10,7 +10,13 @@ using Serilog;
 namespace GitHubExtension;
 
 [ComVisible(true)]
+#if CANARY_BUILD
+[Guid("7AB70F8F-3644-495C-B473-A6750AE1D547")]
+#elif STABLE_BUILD
 [Guid("6B5F1179-B2AE-4D5E-94FC-E5E119D1B8F0")]
+#else
+[Guid("190B5CB2-BBAC-424E-92F8-98C7C41C1039")]
+#endif
 [ComDefaultInterface(typeof(IExtension))]
 public sealed class GitHubExtension : IExtension
 {
