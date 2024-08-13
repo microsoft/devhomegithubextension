541e6f01c6c5c5be568fdd9644b62239f6aaf5b0
Kristen Schau <47155823+krschau@users.noreply.github.com>
2024-08-13 10:36:50 -0400

    fix namespace

--- src/GitHubExtension/Helpers/FileHelper.cs
+++ src/GitHubExtension/Helpers/FileHelper.cs
@@ -4,7 +4,7 @@
 using System.Text;
 using Newtonsoft.Json;
 
-namespace DevHome.Common.Services;
+namespace GitHubExtension.Helpers;
 
 public static class FileHelper
 {
--- src/GitHubExtension/Helpers/LocalSettings.cs
+++ src/GitHubExtension/Helpers/LocalSettings.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using DevHome.Common.Services;
 using Windows.Storage;
 
 namespace GitHubExtension.Helpers;
